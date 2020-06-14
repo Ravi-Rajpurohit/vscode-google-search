faa5e54a2ca7a24ab5467384ef5e3e59f8f1c4a1
Ravi <ravi.rajpurohit385@gmail.com>
2020-06-14 11:40:48 +0530

    📦 NEW: added hello world extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "google-search" is now active!');
@@ -13,12 +12,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('google-search.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand(
+		'google-search.helloWorld',
+		() => {
+			// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
-	});
+			// Display a message box to the user
+			vscode.window.showInformationMessage('Hello from the other side !');
+		}
+	);
 
 	context.subscriptions.push(disposable);
 }
