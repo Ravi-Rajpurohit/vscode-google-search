28dddf85f400211774a9c2119c04120234f25076
Ravi <ravi.rajpurohit385@gmail.com>
2020-06-14 20:33:38 +0530

    📦 NEW: added new feature to search from terminal

--- src/extension.ts
+++ src/extension.ts
@@ -12,17 +12,66 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
+	vscode.window.showInformationMessage('onStartupFinished');
+
+	vscode.workspace
+		.getConfiguration()
+		.update(
+			'terminal.integrated.copyOnSelection',
+			true,
+			vscode.ConfigurationTarget.Global
+		);
+
 	let disposable = vscode.commands.registerCommand(
-		'google-search.helloWorld',
+		'google-search.makeSearch',
 		() => {
 			// The code you place here will be executed every time your command is executed
 
 			// Display a message box to the user
-			vscode.window.showInformationMessage('Hello from the other side !');
+
+			vscode.workspace
+				.getConfiguration()
+				.update(
+					'terminal.integrated.copyOnSelection',
+					true,
+					vscode.ConfigurationTarget.Global
+				);
+
+			const activeTextEditor = vscode.window.activeTextEditor;
+
+			if (activeTextEditor) {
+				const selectedText = activeTextEditor.document.getText(
+					activeTextEditor.selection
+				);
+
+				vscode.window.showInformationMessage(selectedText);
+
+				vscode.env.openExternal(
+					vscode.Uri.parse(`https://www.google.com/search?q=${selectedText}`)
+				);
+			}
+		}
+	);
+
+	let terminalDisposable = vscode.commands.registerCommand(
+		'google-search.makeSearchFromTerminal',
+		async () => {
+			// The code you place here will be executed every time your command is executed
+
+			// Display a message box to the user
+			const selectedText = await vscode.env.clipboard.readText();
+
+			vscode.window.showInformationMessage(selectedText);
+
+			vscode.env.openExternal(
+				vscode.Uri.parse(`https://www.google.com/search?q=${selectedText}`)
+			);
 		}
 	);
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(terminalDisposable);
 }
 
 // this method is called when your extension is deactivated
